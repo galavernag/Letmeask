6916d25ecd51013d6113bcb1578515a08b181bb6
Guigalaverna <guigalaverna@gmail.com>
2021-06-21 20:26:13 -0300

    Importing and Exporting images from assets folder

---
+++ src/assets/index.ts
@@ -0,0 +1,21 @@
+import Illustration from './images/illustration.svg'
+import Answer from './images/answer.svg'
+import Check from './images/check.svg'
+import Copy from './images/copy.svg'
+import Delete from './images/delete.svg'
+import EmptyQuestions from './images/empty-questions.svg'
+import GoogleIcon from './images/google-icon.svg'
+import Like from './images/like.svg'
+import Logo from './images/logo.svg'
+
+export {
+  Answer,
+  Check,
+  Copy,
+  Delete,
+  EmptyQuestions,
+  GoogleIcon,
+  Illustration,
+  Like,
+  Logo
+}
