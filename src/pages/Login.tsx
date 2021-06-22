7c3cf4310f02d94fd4acdf52f4db7673e5e751f8
Guigalaverna <guigalaverna@gmail.com>
2021-06-21 21:33:05 -0300

    Created navigation function

---
+++ src/pages/Login.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { GoogleIcon, Illustration, Logo } from '../assets'
+import { Button } from '../components';
+
+import '../styles/auth.scss'
+
+function Home() {
+  const history = useHistory()
+
+  function navigateToCreateRoom() {
+    history.push('/create-room')
+  }
+
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img src={Illustration} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo.</strong>
+        <p>Tire dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={Logo} alt="Let me ask" />
+          <button className='create-room' onClick={navigateToCreateRoom}>
+            <img src={GoogleIcon} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className='separator'>ou entre em uma sala</div>
+          <form>
+            <input 
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
