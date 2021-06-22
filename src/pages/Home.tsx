9fae4c7501c0f17c5a4806a06ecd6688b337acb2
Guigalaverna <guigalaverna@gmail.com>
2021-06-21 21:17:34 -0300

    Using Button component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 
 import { GoogleIcon, Illustration, Logo } from '../assets'
+import { Button } from '../components';
+
+import '../styles/auth.scss'
 
 function Home() {
   return (
-    <div>
+    <div id='page-auth'>
       <aside>
         <img src={Illustration} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo.</strong>
         <p>Tire dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className='main-content'>
           <img src={Logo} alt="Let me ask" />
-          <button>
+          <button className='create-room'>
             <img src={GoogleIcon} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className='separator'>ou entre em uma sala</div>
           <form>
             <input 
               type="text"
               placeholder="Digite o código da sala"
             />
-            <button type="submit">Entrar na sala</button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
