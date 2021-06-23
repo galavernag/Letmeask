6479feb2b99456d17fa17eb9564d3eab55f7fe9c
Guigalaverna <guigalaverna@gmail.com>
2021-06-23 02:26:44 -0300

    Fetch questions from database

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useRef, useState } from "react"
+import { useEffect } from "react"
+import { FormEvent, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Avatar, Logo } from "../assets"
 import { Button, RoomCode } from "../components"
@@ -11,12 +12,57 @@ interface RoomParams {
   id: string
 }
 
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string,
+    avatar: string
+  },
+  content: string,
+  isAnswered: boolean
+  isHighlighted: boolean
+}>
+
+interface Question {
+  id: string
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isAnswered: boolean
+  isHighlighted: boolean
+}
+
 function Room() {
   const { user } = useAuth()
   const params = useParams<RoomParams>()
   const roomId = params.id
 
   const [newQuestion, setNewQuestion] = useState('')
+  const [questions, setQuestions] = useState<Question[]>([])
+  const [title, setTitle] = useState('')
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`)
+
+    roomRef.on('value', (room) => {
+      const databaseRoom = room.val()
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isAnswered: value.isAnswered,
+          isHighlighted: value.isHighlighted
+        }
+      })
+
+      setTitle(databaseRoom.title)
+      setQuestions(parsedQuestions)
+    })
+  }, [roomId])
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault()
@@ -40,6 +86,8 @@ function Room() {
     }
 
     await database.ref(`rooms/${roomId}/questions`).push(question)
+
+    setNewQuestion('')
   }
 
   return (
@@ -52,8 +100,16 @@ function Room() {
       </header>
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala - {title}</h1>
+          {questions.length > 0 && (
+            <>
+              {questions.length > 1 ? (
+                <span>{questions.length} perguntas</span>
+              ) : (
+                <span>{questions.length} pergunta</span>
+              )}
+            </>
+          )}
         </div>
         <form>
           <textarea
@@ -67,7 +123,7 @@ function Room() {
               <span>Para enviar uma pergunta, <button>faça seu login</button></span>
             ) : (
               <div className="user-info">
-                <img src={ !user.avatar ? Avatar : user.avatar } alt={user.name} />
+                <img src={!user.avatar ? Avatar : user.avatar} alt={user.name} />
                 <span>{user.name}</span>
               </div>
             )}
