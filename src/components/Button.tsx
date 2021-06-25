9a1a2e0ab4dd6e6e50db4522cc775e167330741a
Guigalaverna <guigalaverna@gmail.com>
2021-06-25 04:43:53 -0300

    Add outlined button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,16 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import '../styles/button.scss'
 
-function Button({ ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isOutlined?: boolean
+}
+
+function Button({ isOutlined = false, ...rest }: ButtonProps) {
   return (
-    <button className='button' {...rest}></button>
+    <button 
+      className={`button ${isOutlined ? 'outlined' : ''}`}
+      {...rest}
+    />
   )
 }
 
